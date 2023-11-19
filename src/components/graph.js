f496a2c28013d664a06e4a9441d98e36c727127c
priming <web2ajax@gmail.com>
2023-11-19 00:18:30 +0000

    update

--- src/components/graph.js
+++ src/components/graph.js
@@ -5,29 +5,20 @@ import Graph from 'react-vis-network-graph'
 // Função para importar todas as imagens da pasta 'img'
 const importAll = (context) => context.keys().map(context);
 
-// Importa todas as imagens da pasta 'img'
-const images = importAll(require.context('../img/', false, /\.(png|jpe?g|svg)$/));
-
-var DIR = "../img/";
-
 export default function GraphView() {
 
     const graph = {
         nodes: [
-            {id: 1, label: "Node 1", title: "node 1 tooltip text",
-            shape: "box"},
-            {id: 2, label: "Node 2", title: "node 2 tooltip text",
-            shape: "circle"},
-            {id: 3, label: "Node 3", title: "node 3 tooltip text",
-            shape: "circle"},
-            {id: 4, label: "Node 4", title: "node 4 tooltip text",
-            shape: "circle"},
-            {id: 5, label: "Node 5", title: "node 5 tooltip text", shape: "circle", image: DIR + "eu.png"},
-            {id: 6, label: "Node 6", title: "node 6 tooltip text", shape: "circle", image: DIR + "eu.png"},
-            {id: 7, label: "Node 7", title: "node 7 tooltip text", shape: "circle", image: images[0]},
-            {id: 8, label: "Node 8", title: "node 8 tooltip text"},
+            {id: 1, label: "Dog", title: "Dog", shape: "circularImage", image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS90CXTEr2iyiUvjnQnoIt2nBVDzixFy-jn1sz0C-S73RO7kfVWEBAhiZCm9POv"},
+            {id: 2, label: "Hause", title: "Hause", shape: "circularImage", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTeSH-YRXQG6pzdmRzn68Sz9oKdEsjfH8vdO0uv0X67FxSc0X8wsT_xhA8nLdbd"},
+            {id: 3, label: "Airplane", title: "Airplane", shape: "circularImage", image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSG-lCrVAstpixSkLoXqqBCcduNQmydLOYE4JyXVHsqjahjkYH9xCUoGmhh4I4P"},
+            {id: 4, label: "Node 4", title: "node 4 tooltip text", shape: "circularImage", image: "eu.png"},
+            {id: 5, label: "Node 5", title: "node 5 tooltip text", shape: "circularImage", image: "eu.png"},
+            {id: 6, label: "Node 6", title: "node 6 tooltip text", shape: "circularImage", image: "eu.png"},
+            {id: 7, label: "Cat", title: "Cat", shape: "circularImage", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSag4rs4bgceWDF5hY6KaZeu3HerZ_dCvY_gYdOu0yIDQIrwfL8wOC5No-q81t"},
+            {id: 8, label: "Node 8", title: "node 8 tooltip text", shape: "circularImage", image: "eu.png"},
             {id: 9,title: "Have", shape: "circularImage", image: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/6666f8bf1d38ded1fc49965fc02fb379.jpg?imageView2/2/w/180/q/70"},
-            {id: 10,title: "Love", shape: "circularImage", image: images[0]}
+   
         ],
         edges: [
             {from: 1, to: 1, smooth: {type: "curvedCW"}, arrows: {from: {enabled: true, type: "circle"}, to: {enabled: true, type: "circle"}}},
